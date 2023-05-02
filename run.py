5ad55ef4e047dfe5c7e7aecd65f9e850fc08051d
Mats Gustafsson <mats.gustafsson83@gmail.com>
2023-05-02 22:56:02 +0000

    Setup the board, rows and cols. adding little ascii logo.

---
+++ run.py
@@ -0,0 +1,21 @@
+from random import randint
+
+# creates and clean board 
+board = []
+
+# adding the rows in board. 
+for i in range(0, 8):
+    board.append(["O"] *8)
+    
+# function to print the board rows.
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+
+# function to display to logo. 
+def print_logo():
+    logo = .devcontainer/
+
+
+
+    
