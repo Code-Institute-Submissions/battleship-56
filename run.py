458f59f1e9185349f1c0c1e9d3721e4c2344ef96
Moonchichiii <>
2023-05-02 21:29:47 +0000

    Imports and setup the board and tested it so far.

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#imports 
+import os
+from random import randint
+
+# the board setup 
+board = []
+
+for i in range(0, 8):
+    board.append(["O"] *8)
+
+# Printing out the board 
+def print_board(board):
+    for row in board:
+        print("  ".join(row))
+
+
+# Gamme instructions and username prompt 
+
+def print_logo():
+    logo = """
+    """
+    print(logo)
+
+print_logo()
+print("\n")
+print("Welcome to Battleship!\n")
