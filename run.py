01e52ceaa6646ddba4e86c72f940eb7eda106f99
Mats Gustafsson <mats.gustafsson83@gmail.com>
2023-05-02 23:17:09 +0000

    Added the random rows and coloumns, and set the number of ships to be found inside the loop

--- run.py
+++ run.py
@@ -28,6 +28,47 @@ def print_logo():
 
 print_logo()
 
+# game instructions 
+print("Welcome to BattleShip!\n")
+print("You got ten attempts to sink the all 3 ships!")
+print("If you get bored after a few attempts, just press Enter to exit\n")
+
+username = input("Please select a username: ")
+
+print(f"Let's play BattleShip, {username.upper()}!\n")
+print_board(board)
+
+# function for random rows
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+# function for random columns
+def random_col(board):
+    return randint(0, len(board) - 1)
+
+
+# number of ships and the locations. 
+
+num_ship = 3
+ship_location = [(random_row(board),random_col(board)) for _ in range(num_ship)]
+
+# Ships found. 
+ships_found = 0
+
+# main game loop 
+turn = 0
+while True:
+    print("\nTurn", turn + 1)
+
+    
+
+
+
+
+
+
+
 
 
 
